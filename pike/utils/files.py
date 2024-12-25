3fe07dae033f55416867cd0857f87a27fbcfd7ef
skelmis <skelmis.craft@gmail.com>
2024-12-26 01:17:40 +1300

    feat: allow empty variables file

--- pike/utils/files.py
+++ pike/utils/files.py
@@ -1,13 +1,25 @@
 import json
+import logging
 from pathlib import Path
 from typing import Any
 
 import frontmatter
 
+log = logging.getLogger(__name__)
 
-def read_file_as_json(file: Path | str) -> dict[str, Any]:
-    with open(file, "r") as f:
-        data = json.load(f)
+
+def read_file_as_json(
+    file: Path | str, *, allow_missing: bool = False
+) -> dict[str, Any]:
+    try:
+        with open(file, "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        if allow_missing:
+            log.warning("Could not find file %s but defaulting to empty dict", file)
+            return {}
+
+        raise
 
     return data
 
