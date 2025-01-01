7719163394d59fd32eee1b218b9e4490f8f971b5
skelmis <skelmis.craft@gmail.com>
2025-01-02 00:44:03 +1300

    Formatting

--- tests/test_jinja.py
+++ tests/test_jinja.py
@@ -9,6 +9,7 @@ def test_jinja_configs(engine: Engine):
     with pytest.raises(jinja2.exceptions.UndefinedError):
         engine.jinja_env.from_string("{{missing}}").render()
 
+
 def test_insert_image(engine: Engine):
     r_1 = insert_image("cat.jpg")
     assert r_1 == "<img src='cat.jpg'>"
@@ -16,5 +17,10 @@ def test_insert_image(engine: Engine):
     r_2 = insert_image("cat.jpg", width=100)
     assert r_2 == "<img src='cat.jpg' width='100'>"
 
-    r_3 = insert_image("cat.jpg", width=100, height=100, alt_text="Alt Text", caption="Caption")
-    assert r_3  == "<img src='cat.jpg' alt='Alt Text' width='100' height='100' title='Caption'>"
+    r_3 = insert_image(
+        "cat.jpg", width=100, height=100, alt_text="Alt Text", caption="Caption"
+    )
+    assert (
+        r_3
+        == "<img src='cat.jpg' alt='Alt Text' width='100' height='100' title='Caption'>"
+    )
